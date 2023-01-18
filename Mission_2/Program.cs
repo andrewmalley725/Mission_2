fe408b8ebe86b066c25ae64967c809cd399b8d71
andyman <andyman@ANDREWALLEYBB44>
2023-01-18 16:29:21 -0700

    Add project files.

---
+++ Mission_2/Program.cs
@@ -0,0 +1,29 @@
+﻿//this was written using .NET 5.0
+
+using System;
+
+namespace Mission_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.Write("\nHow many dice rolls would you like to simulate? ");
+
+            string numRolls = Console.ReadLine();
+            int rolls = Convert.ToInt32(numRolls);
+
+            Throw th = new Throw(rolls);
+
+            Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + numRolls + ".\n");
+
+            th.Play(); //call Play method from throw class
+
+            Console.WriteLine("\nThank you for using the dice throwing simulator. \tGoodbye!");
+        }
+    }
+}
+
---
+++ Mission_2/Throw.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_2
+{
+    public class Throw
+    {
+        private int NumRolls = 0;
+
+        public Throw(int rolls)
+        {
+            NumRolls = rolls;
+        }
+
+        public void Play()
+        {
+            var rand = new Random();
+            int[] totals = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; //array stores the number of roles for each possible value 2-12
+            double[] percents = new double[totals.Length]; //percents array stores the pecentage of each value
+            int dice1 = 0;
+            int dice2 = 0;
+            int dieSum = 0; //stores the sum of the two die
+            int bigSum = 0; //stores the sum of the totals array
+
+            for (int i = 0; i < NumRolls; i++)
+            {
+                dice1 = rand.Next(1, 7);
+                dice2 = rand.Next(1, 7);
+
+                dieSum = dice1 + dice2;
+
+                totals[dieSum - 2] = totals[dieSum - 2] + 1;
+
+            }
+
+            bigSum = totals.Sum();
+
+            for (int i = 0; i < totals.Length; i++)
+            {
+                percents[i] = Math.Round(Convert.ToDouble(totals[i]) / bigSum * 100);
+                int percent = (Int32)percents[i];
+                IEnumerable<string> ast = Enumerable.Repeat("*", percent);
+                Console.Write((i + 2).ToString() + ": ");
+
+                foreach (string str in ast)
+                {
+                    Console.Write(str);
+                }
+
+                Console.Write("\n");
+            }
+
+        }
+    }
+}
